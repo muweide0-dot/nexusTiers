07b4a0b6f7310ef1f2304f7e4200588fe0660fbc
muweide0-dot <muweide0@gmail.com>
2026-09-22 20:51:31 +0200

    Sync Discord bot updates: artifacts/api-server/src/index.ts

--- artifacts/api-server/src/index.ts
+++ artifacts/api-server/src/index.ts
@@ -1,6 +1,6 @@
 import app from "./app";
+import { startDiscordBot } from "./discord-bot";
 import { logger } from "./lib/logger";
-import { startDiscordBot } from "./lib/discord-bot";
 
 const rawPort = process.env["PORT"];
 
@@ -23,5 +23,6 @@ app.listen(port, (err) => {
   }
 
   logger.info({ port }, "Server listening");
-  startDiscordBot();
 });
+
+startDiscordBot();
